06cb8c9560fc1d2c83358122824869d199191d9a
Johnny Proano <djimageonline@gmail.com>
2022-12-15 00:17:50 -0500

    added the productsindex component and connected the backend with axios

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,24 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { ProductsIndex } from "./ProductsIndex";
+
 export function Home() {
+  const [products, setProducts] = useState([]);
+
+  const handleIndexPhotos = () => {
+    console.log("handleIndexPhotos");
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(handleIndexPhotos, []);
+
   return (
     <div>
       <h1>Welcome ShopR</h1>
+      <ProductsIndex products={products} />
     </div>
   );
 }
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,15 @@
+export function ProductsIndex(props) {
+  return (
+    <div>
+      <h2>All Products</h2>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p> Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+          {/* <img src={product.url}></img> */}
+        </div>
+      ))}
+    </div>
+  );
+}
